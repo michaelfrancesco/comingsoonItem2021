2d6cb94fa5ab8a25ad17ba85871f9235468b115f
Ivan <c14200138@john.petra.ac.id>
2021-03-10 12:44:23 +0900

    bug fix

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
             <div class="modal-header">
               <h5 class="modal-title" id="myModalLabel">Movie Quote</h5>
                 <button type="button" class="btn-close btn-close-white" data-bs-dismiss="modal" aria-label="Close">
-                  <span aria-hidden="true">&times;</span>
+                  <!-- <span aria-hidden="true">&times;</span> -->
                 </button>
             </div>
             <div class="modal-body">
